c452f0f9130714c1e6f703777a6a8deaea0d84ca
chana <chana987@gmail.com>
2023-02-22 13:23:31 +0200

    changed server

--- payload/src/server.ts
+++ payload/src/server.ts
@@ -1,19 +1,12 @@
 import express from 'express';
 import payload from 'payload';
-import cors from 'cors';
 
+require('dotenv').config();
 const app = express();
-app.use(cors({ origin: true }));
 
 // Redirect root to Admin panel
 app.get('/', (_, res) => {
   res.redirect('/admin');
-});
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
 });
 
 const start = async () => {
@@ -23,13 +16,13 @@ const start = async () => {
     mongoURL: process.env.MONGODB_URI,
     express: app,
     onInit: async () => {
-      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
     },
-  })
+  });
 
   // Add your own express routes here
 
   app.listen(3000);
-}
+};
 
 start();
